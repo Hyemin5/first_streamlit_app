c7b9b3e35595291453e341073dec8e3baf262f47
mz-hmoh <86704460+Hyemin5@users.noreply.github.com>
2022-12-08 16:33:07 +0900

    Update streamlit_app.py
    response to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🫐 Omega 3 & Blueberry Oatmeal')
@@ -25,3 +25,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
